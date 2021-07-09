e87a95e5d3137ef5a9f4fbff93c8e9fb18f679fb
Drin Ramadani <38039787+drinramadani-code@users.noreply.github.com>
2021-07-09 14:10:58 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
+function resize_navbar_li() {
+  let width = (100 / $("nav#navbar ul.navbar_ul_items li").length) - 5;
+  // $("i.bar_icon").hide()
+  if (!$("i.bar_icon").is(":visible")) {
+    $("nav#navbar ul.navbar_ul_items li").css("width", `${width}%`)
+  }
+}
+
 $(window).on("resize", function() {
   if ($(window).width() >= 1024) {
     $("#navbar > .container > ul").show(300);
+    resize_navbar_li();
   } else {
     $("#navbar > .container > ul").hide(300);
+    $("nav#navbar ul.navbar_ul_items li").css("width", 100);
   }
 });
 
@@ -24,6 +34,8 @@ $(document).on('click', "#add_product_toggler", function() {
 
 $(document).ready(function() {
   let width = (100 / $("nav#navbar ul.navbar_ul_items li").length) - 5;
-  console.log(width);
-  $("nav#navbar ul.navbar_ul_items li").css("width", `${width}%`)
+  // $("i.bar_icon").hide()
+  if (!$("i.bar_icon").is(":visible")) {
+    $("nav#navbar ul.navbar_ul_items li").css("width", `${width}%`)
+  }
 })
