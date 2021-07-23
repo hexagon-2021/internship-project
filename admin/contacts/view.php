706b06e663739d5cdf40a4dcb1396576b34ab8d5
Agron Pacolli <35370237+gitagron@users.noreply.github.com>
2021-07-23 09:36:25 +0200

    Contact files added

---
+++ admin/contacts/view.php
@@ -0,0 +1,52 @@
+<?php require_once("../../includes/dbh.inc.php") ?>
+
+<table class="approved_businesses_table">
+  <?php 
+    $sql = "SELECT * FROM contact_us";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+  ?>
+    <tr>
+      <th>Emri</th>
+      <th>E-mail</th>
+      <th>Mesazhi</th>
+    </tr>
+    <?php 
+        while ($row = mysqli_fetch_assoc($result)) {
+          echo "<tr>";
+            echo "<th>". $row['name'] ."</th>";
+            echo "<th>". $row['email'] ."</th>";
+            echo "<th>". $row['message'] ."</th>";
+          echo "</tr>";
+        }
+    ?>
+  <?php } else {
+    echo "<h1 style='color: var(--secondary-color);text-align: center;'>Nuk keni asnje mesazh!</h1>";
+  } ?>
+</table>
+
+<!--<script>
+  
+    $(".delete_business_action").on('click', function(e) {
+      
+      if(confirm("A jeni i sigurt?")){
+    //e.preventDefault();
+    let id = this.value;
+    
+    $.ajax({
+      url: "approved/action.php",
+      type: "POST",
+      data: {
+        id: id,
+      },
+      success: function(data) {
+  
+        $(".display_approved_businesses").load("approved/view.php");
+
+      }
+    }) 
+    }
+  })
+  
+  
+</script> -->
