f514a0adcf3cf2675247480ea46f4c0afa967eb1
artrrustemi <85810156+artrrustemi@users.noreply.github.com>
2021-07-23 12:37:09 +0200

    added view all products function
    added 3 new view all products function at super admin dashboard

---
+++ admin/products/action.php
@@ -0,0 +1,7 @@
+<?php 
+  require_once('../../includes/dbh.inc.php');
+
+  if (isset($_POST['id'])) {
+    $id = (int) $_POST['id'];
+    mysqli_query($conn, "DELETE FROM product WHERE id=$id LIMIT 1");
+  } 
---
+++ admin/products/view.php
@@ -0,0 +1,61 @@
+<?php require_once("../../includes/dbh.inc.php") ?>
+
+<table class="approved_businesses_table">
+
+	<?php 
+    $sql = "SELECT * FROM product  "; 
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+  ?>
+
+  <tr>
+      <th>Emri</th>
+      <th>Qmimi</th>
+      <th>Përbërësit</th>
+      <th>Kategoria</th>
+      <th>Shikueshmëria</th>
+      <th>Foto</th>
+      <th>Fshij produktin</th>
+    </tr>
+
+    <?php 
+        while ($row = mysqli_fetch_assoc($result)) {
+          echo "<tr>";
+            echo "<th>". $row['item_name'] ."</th>";
+            echo "<th>". $row['item_price'] ."</th>";
+            echo "<th>". $row['item_ingridients'] ."</th>";
+            echo "<th>". $row['item_categorie'] ."</th>";
+            echo "<th>". $row['item_views'] ."</th>";
+            echo "<th><img width='50 ' src='../includes/images/profile_pictures/". $row['item_picture'] ."'></th>";
+            echo "<th><button class='delete_product_action' value='".$row['id']."' id='delete_products'><i class='fas fa-times-circle'></i></button></th>";
+          echo "</tr>";
+        }
+    ?>
+    <?php } else {
+    echo "<h1 style='color: var(--secondary-color);text-align: center;'>Nuk ka asnjë produkt!</h1>";
+  } ?>
+</table>
+
+<script>
+  
+    $(".delete_product_action").on('click', function(e) {
+      
+      if(confirm("A jeni i sigurt?")){
+    //e.preventDefault();
+    let id = this.value;
+    
+    $.ajax({
+      url: "products/action.php",
+      type: "POST",
+      data: {
+        id: id,
+      },
+      success: function(data) {
+  
+        $(".display_approved_businesses").load("products/view.php");
+
+      }
+    }) 
+    }
+  })
+  </script>
