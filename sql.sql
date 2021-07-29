a7e49548332166e177f4baeb4bd48701e1039fd5
Drin Ramadani <38039787+drinramadani-code@users.noreply.github.com>
2021-07-29 16:03:10 +0200

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -51,3 +51,5 @@ ALTER TABLE `product`
 ALTER TABLE `business` ADD `aproved` TINYINT(1) NOT NULL AFTER `phone_number`;
 
 ALTER TABLE `business` ADD `document_name` VARCHAR(256) NOT NULL AFTER `phone_number`;
+
+ALTER TABLE `business` ADD `status` VARCHAR(256) NOT NULL AFTER `document_name`;
