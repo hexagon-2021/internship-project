ddad8a81fcbccdd1f5f7cbdd3317ab4609b47af5
Drin Ramadani <38039787+drinramadani-code@users.noreply.github.com>
2021-08-03 14:22:38 +0200

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,40 @@ require_once('includes/functions.inc.php');
 </head>
 <body>
 
-  <?php $active="Home"; include 'includes/nav.inc.php'; ?>
+  <?php $active="Ballina"; include 'includes/nav.inc.php'; ?>
+  
+  <div id="home_page">
+    <div class="clickbait">
+      <div class="row">
+        <div class="col-4 clickbait_item">
+          <i class="fas fa-search-location"></i>
+          <label>Zgjedh Qytetin Tuaj</label>
+        </div>
+        <div class="col-4 clickbait_item">
+          <i class="fas fa-shopping-basket"></i>
+          <label>Zgjedh Ushqimin</label>
+        </div>
+        <div class="col-4 clickbait_item">
+          <i class="fas fa-money-bill-wave"></i>
+          <label>Paguaj Si Te Duash</label>
+        </div>
+      </div> <!-- row -->
+    </div> <!-- clickbait-->
+    <div id="choose_city">
+      <h3>Zgjedh Qytetin</h3>
+      <div class="cities_to_select">
+        <button class='city_selectable'><label>1</label>. Prishtinë</button>
+        <button class='city_selectable'><label>2</label>. Mitrovicë</button>
+        <button class='city_selectable'><label>3</label>. Pejë</button>
+        <button class='city_selectable'><label>4</label>. Prizren</button>
+        <button class='city_selectable'><label>5</label>. Ferizaj</button>
+        <button class='city_selectable'><label>6</label>. Gjilan</button>
+        <button class='city_selectable'><label>7</label>. Gjakovë</button>
+      </div>
+    </div>
+    <!-- <img src="https://www.recipetineats.com/wp-content/uploads/2020/02/Chicken-Burritos_2.jpg?w=500&h=500&crop=1" alt="burrito" /> -->
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/RedDot_Burger.jpg/1200px-RedDot_Burger.jpg" alt="hamburger" />
+  </div> <!-- #home_page -->
 
   <script src="https://kit.fontawesome.com/7071bdd24d.js" crossorigin="anonymous"></script>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
