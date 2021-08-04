09d95a4032a6e60e8ce5c94e2d1edc7bfcbc47b7
artrrustemi <85810156+artrrustemi@users.noreply.github.com>
2021-08-04 13:37:04 +0200

    Update index.php

--- view_products/index.php
+++ view_products/index.php
@@ -1,1 +1,21 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	 <link rel="stylesheet" type="text/css" href="../css/style.css" />
+	 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	 <style type="text/css">
+	 	body {
+  			background-color: var(--fourth-color);
+		}
+	 </style>
+</head>
+<body>
+	<?php $active = 'Ballina'; include '../includes/nav.inc.php'; ?>
+	<?php include 'view.php'; ?>
+	 <script src="https://kit.fontawesome.com/7071bdd24d.js" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+  <script src="../js/dashboard.js"></script>
+  <script src="../js/main.js"></script>
+</body>
+</html>
