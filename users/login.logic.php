5f627e0ff5a8764887d7080ef22d4420018eb186
artrrustemi <85810156+artrrustemi@users.noreply.github.com>
2021-08-16 16:41:09 +0200

    Add files via upload

---
+++ users/login.logic.php
@@ -0,0 +1,29 @@
+<?php
+require_once('../includes/functions.inc.php');
+require_once('../includes/session.php');
+	if(isset($_SESSION['realUserid'])){
+		header('location: ../dashboard');
+	}
+	if (isset($_POST["submit"])) {
+	
+		$username = $_POST["uid"];
+		$pwd = $_POST["pwd"];
+
+		require_once '../includes/dbh.inc.php';
+		require_once '../includes/functions.inc.php';
+
+		if ($username == "admin" && $pwd == "admin") {
+			$_SESSION['admin'] = "admin";
+		}
+		
+		if (emptyInputLogin($username, $pwd) !== false) {
+			header("location: ../login.php?error=emptyinput");
+			exit();
+		}
+
+		loginUser($conn, $username, $pwd);
+	}
+	else{
+		header("location: ../login.php");
+		exit();
+	}
