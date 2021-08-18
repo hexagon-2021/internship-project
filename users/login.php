fb5330f972c0319628ea2603a1f0d29ccfa0dd5b
artrrustemi <85810156+artrrustemi@users.noreply.github.com>
2021-08-18 13:14:29 +0200

    Update login.php

--- users/login.php
+++ users/login.php
@@ -15,11 +15,17 @@
 </head>
 <body>
 	<?php $active= "Log In"; include '../includes/nav.inc.php'; ?>	
+
+	<center>
+		<button id="userLogin" value="Kyqu Si Klient" class="ndrroRegjistriminBtn">Kyqu Si Klient</button>
+	</center>
+
+	<div class="formChange" id="formChange-div">
 	
-		<form action="../users/login.logic.php" method="post">
+		<form action="../users/login.logic.php" method="post" id="loginUi" style="display: block;">
 
 			<div class="login-items">
-				<h1 class="login">Users Log In</h1>
+				<h1 class="login">Kyqja per Biznes</h1>
 			<input type="text" name="uid" placeholder="Username/Email..." class="inputL"><br>
 			<input type="password" name="pwd" placeholder="Password..." class="inputL"><br>
 			<button type="submit" name="submit" class="signinSignupButton">Log in</button>
@@ -36,11 +42,37 @@
 			?>
 		</div>
 		</form>
-		<div class="dontHaveAnAcc">
-			<h1>Don't have an account? <a href="signupForm.php">Sign up</a></h1>
+		<div class="dontHaveAnAcc" id="dontHaveAnAccUi" style="display: block;">
+			<h1>Don't have an account? <a href="index.php">Sign up</a></h1>
+		</div>
+
+	
+
+		<form action="../includes/login.inc.php" method="post" id="loginBu">
+
+			<div class="login-items">
+				<h1 class="login">Kyqja per Klient</h1>
+			<input type="text" name="uid" placeholder="Username/Email..." class="inputL"><br>
+			<input type="password" name="pwd" placeholder="Password..." class="inputL"><br>
+			<button type="submit" name="submit" class="signinSignupButton">Log in</button>
+		<?php
+				if (isset($_GET["error"])) {
+					if ($_GET["error"] == "emptyinput") {
+						echo "<p>Fill in all fields!</p>";
+					}
+					else if ($_GET["error"] == "wronglogin") {
+						echo "<p>Incorrect login information!</p>";
+					}
+				}
+	
+			?>
+		</div>
+		</form>
+		<div class="dontHaveAnAcc" id="dontHaveAnAccBu">
+			<h1>Don't have an account? <a href="index.php">Sign up</a></h1>
 		</div>
 	
-	
+	</div>
 	<script src="https://kit.fontawesome.com/7071bdd24d.js" crossorigin="anonymous"></script>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
   <script src="../js/main.js"></script>
