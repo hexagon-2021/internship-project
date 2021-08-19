c4451d0c35ef5914dfa6fed0792c1e348f325c53
artrrustemi <85810156+artrrustemi@users.noreply.github.com>
2021-08-19 15:35:27 +0200

    Update index.php

--- users/index.php
+++ users/index.php
@@ -26,35 +26,35 @@
 	<form enctype="multipart/form-data" action="action.php" method="post" id="registerUi" style="display: block;">
 		<div class="signup-items">
 			<h1 class="signup">Regjistrimi Per Klient</h1>
-			<input type="text" name="name" placeholder="Full name..." class="input">
+			<input type="text" name="name" placeholder="Emri i plot..." class="input">
 			<input type="text" name="email" placeholder="Email..." class="input">
-			<input type="text" name="username" placeholder="Username..." class="input">
-			<input type="password" name="pwd" placeholder="Password..." class="input">
-			<input type="password" name="pwdRepeat" placeholder="Repeat password..." class="input">
-			<input type="text" name="phone_number" placeholder="Phone number..." class="input">
-			<button type="submit" name="submit" class="pointer signinSignupButton">Sign up</button>
+			<input type="text" name="username" placeholder="Emri..." class="input">
+			<input type="password" name="pwd" placeholder="Fjalkalimi..." class="input">
+			<input type="password" name="pwdRepeat" placeholder="Perserit fjalkalimin..." class="input">
+			<input type="text" name="phone_number" placeholder="Numri i telefonit..." class="input">
+			<button type="submit" name="submit" class="pointer signinSignupButton">Regjistrohu</button>
 			<?php
 				if (isset($_GET["error"])) {
 					if ($_GET["error"] == "emptyinput") {
-						echo "<p>Fill in all fields!</p>";
+						echo "<p>Mbushni te gjitha fushat!</p>";
 					}
 					else if ($_GET["error"] == "invalidemail") {
-						echo "<p>Choose a proper email!</p>";
+						echo "<p>Zgjidhni nje email te rregullt!</p>";
 					}
 					else if ($_GET["error"] == "invaliduid") {
-						echo "<p>Choose a proper username!</p>";
+						echo "<p>Zgjidhni nje emer te rregullt!</p>";
 					}
 					else if ($_GET["error"] == "passwordsdontmatch") {
-						echo "<p>Password doesn't match!</p>";
+						echo "<p>Fjalkalimi nuk pershtatet!</p>";
 					}
 					else if ($_GET["error"] == "usernametaken") {
-						echo "<p>Username already taken!</p>";
+						echo "<p>Emri i perdoruesit ekziston!</p>";
 					}
 					else if ($_GET["error"] == "stmtfailed") {
-						echo "<p>Some thing went wrong, try again!</p>";
+						echo "<p>Diqka shkoj gabim , provo perseri!</p>";
 					}
 					else if ($_GET["error"] == "none") {
-						echo "<p>You have signed up!</p>";
+						echo "<p>Ju jeni regjistruar!</p>";
 					} 
 					else if ($_GET['error'] == "FjalkalimiDuhetTiKetSëPaku8Karaktere") {
 						echo "<p>Fjalkalimi duhet ti ket së paku 8 karaktere , të përmbaj një shkronjë të madhe një të vogël dhe një numër!</p>";
@@ -64,7 +64,7 @@
 		</div>
 	</form>
 		<div class="haveanAcc" id="haveanAccUsrers" style="display: block;">
-			<h1>Have an account? <a href="login.php">Log in</a></h1>
+			<h1>Keni llogari? <a href="login.php">Regjistrohu</a></h1>
 		</div>
 
 
@@ -73,42 +73,42 @@
 		<form enctype="multipart/form-data" action="../includes/signup.inc.php" method="post" id="registerBu" >
 		<div class="signup-items">
 			<h1 class="signup">Regjistrimi Per Biznes</h1>
-			<input type="text" name="name" placeholder="Full name..." class="input">
+			<input type="text" name="name" placeholder="Emri i plot..." class="input">
 			<input type="text" name="email" placeholder="Email..." class="input">
-			<input type="text" name="username" placeholder="Username..." class="input">
-			<input type="password" name="pwd" placeholder="Password..." class="input">
-			<input type="password" name="pwdRepeat" placeholder="Repeat password..." class="input">
-			<input type="text" name="companyName" placeholder="Company name..." class="input">
-			<input type="text" name="companyCity" placeholder="Company city..." class="input">
-			<input type="text" name="phone_number" placeholder="Phone number..." class="input">
+			<input type="text" name="username" placeholder="Emri..." class="input">
+			<input type="password" name="pwd" placeholder="Fjalkalimi..." class="input">
+			<input type="password" name="pwdRepeat" placeholder="Perserit fjalkalimin..." class="input">
+			<input type="text" name="companyName" placeholder="Emri i kompanis..." class="input">
+			<input type="text" name="companyCity" placeholder="Qyteti i kompanis..." class="input">
+			<input type="text" name="phone_number" placeholder="Numri i telefonit..." class="input">
 			<input type="file" name="document" placeholder="Dokumenti per vertetim te biznesit" class="input">
 			<span style="color: red;font-size: 20px;">Ju lutem te bashkangjitni nje file te formatit PDF, DOCX ose nje foto e cila verteton pronesine e biznesit tuaj!</span>
-			<button type="submit" name="submit" class="pointer signinSignupButton">Sign up</button>
+			<button type="submit" name="submit" class="pointer signinSignupButton">Regjistrohu</button>
 			<?php
 				if (isset($_GET["error"])) {
 					if ($_GET["error"] == "emptyinput") {
-						echo "<p>Fill in all fields!</p>";
+						echo "<p>Mbush te gjitha fushat!</p>";
 					}
 					else if ($_GET["error"] == "invalidemail") {
-						echo "<p>Choose a proper email!</p>";
+						echo "<p>Zgjedh nje email te rregullt!</p>";
 					}
 					else if ($_GET["error"] == "invaliduid") {
-						echo "<p>Choose a proper username!</p>";
+						echo "<p>Zgjedh nje emer te rregullt!</p>";
 					}
 					else if ($_GET["error"] == "passwordsdontmatch") {
-						echo "<p>Password doesn't match!</p>";
+						echo "<p>Fjalkalimi nuk pershtatet!</p>";
 					}
 					else if ($_GET["error"] == "usernametaken") {
-						echo "<p>Username already taken!</p>";
+						echo "<p>Emri i perdoruesit ekziston!</p>";
 					}
 					else if ($_GET["error"] == "stmtfailed") {
-						echo "<p>Some thing went wrong, try again!</p>";
+						echo "<p>Diqka shkoj gabim , provoni perseri!</p>";
 					}
 					else if ($_GET["error"] == "none") {
-						echo "<p>You have signed up!</p>";
+						echo "<p>Jeni regjistruar!</p>";
 					}
 					else if ($_GET["error"] == "companyNameExists") {
-						echo "<p>Company name already taken!</p>";
+						echo "<p>Emri i kompanis i zen!</p>";
 					} 
 					else if ($_GET['error'] == "docfs") {
 						echo "<p>Madhesia e file-it eshte shume e madhe!</p>";
@@ -127,7 +127,7 @@
 		</div>
 	</form>
 		<div class="haveanAcc" id="haveanAccBusiness" >
-			<h1>Have an account? <a href="login.php">Log in</a></h1>
+			<h1>Keni llogari? <a href="login.php">Kyqu</a></h1>
 		</div>
 
 
