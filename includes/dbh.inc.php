721f66c582fb1d6b3ae92fc1764ca9966d9e54e8
fatlumxhafaOP <fx48076@universum-ks.org>
2021-08-20 11:36:45 +0200

    configured credentials

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,12 +1,12 @@
 <?php
 
   $server_name = "localhost";
-  $db_username = "root";
-  $db_password = "";
+  $db_username = "hexagon";
+  $db_password = "hexagon12";
   $db_name = "hexagon_db";
 
   $conn = mysqli_connect($server_name, $db_username, $db_password, $db_name);
 
   if (!$conn){
-    die("Conncetion faild: " .mysqli_connect_error());
+    die("Connection failed: " .mysqli_connect_error());
   }
